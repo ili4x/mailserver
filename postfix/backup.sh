465540a079a7ab1485be0d1b114886423638687b
root <root@srv2.orn.ru>
2023-08-29 07:37:54 +0000

    Upload backup

--- postfix/backup.sh
+++ postfix/backup.sh
@@ -8,6 +8,17 @@ fi
 
 find /backup/ -type f -mtime +$BACKUP_DAYS -name '*.gz' -execdir rm -- '{}' \;
 
+FN=backup-`date +"%Y-%m-%d_%H-%M-%S"`.tar.gz
+
+cd /backup
+
 mysqldump --databases mail roundcube > backup.sql
-tar -czf /backup/backup-`date +"%Y-%m-%d_%H-%M-%S"`.tar.gz /var/vmail backup.sql &> /dev/null
+tar -czf /backup/$FN /var/vmail backup.sql &> /dev/null
 rm backup.sql
+
+if [ -n "$BACKUP_UPLOAD_URL" ] ; then
+	curl -k -T $FN $BACKUP_UPLOAD_URL --user $BACKUP_UPLOAD_USER:$BACKUP_UPLOAD_PASSWD
+	if [ "$BACKUP_UPLOAD_DELETE" == "yes" ] ; then
+		rm $FN
+	fi
+fi
